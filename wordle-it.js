5c78c235a8612e36e67cf2ac2e2c363d21edf999
Alphire <37675355+Alphire@users.noreply.github.com>
2022-02-21 20:41:04 +0100

    Aggiornato la tastiera

--- wordle-it.js
+++ wordle-it.js
@@ -39,14 +39,14 @@ let keyboard = new Keyboard({
     default: [
       "q w e r t y u i o p",
       "a s d f g h j k l",
-      "{shift} z x c v b n m {backspace}",
-      "{numbers} {space} {ent}"
+      "z x c v b n m {backspace}",
+      "{space} {ent}"
     ],
     shift: [
       "Q W E R T Y U I O P",
       "A S D F G H J K L",
       "{shift} Z X C V B N M {backspace}",
-      "{numbers} {space} {ent}"
+      "{space} {ent}"
     ],
     numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"]
   },
